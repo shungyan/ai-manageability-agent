8602059541e620c9e04446824ac72b663817ce8d
sysadmin <shungyan@hotmail.my>
2025-07-03 17:07:30 +0800

    1. added quixstreams into requirements.txt 2. added kafka_consumer.py by huichun

--- kafka consumer agent/agent.py
+++ kafka consumer agent/agent.py
@@ -1,17 +1,9 @@
-# from google.adk.agents import Agent
 from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from .consumer import consume
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioConnectionParams, StdioServerParameters
 
 ollama_model = LiteLlm(model="ollama_chat/llama3.2")
-
-# root_agent = Agent(
-#     name = "OllamaLocalAgent",
-#     model = ollama_model,
-#     description = "An agent powered by Ollama local model",
-#     instruction = "You are a helpful assistant.",
-#     tools = []
-# )
 
 root_agent = LlmAgent(
     model=LiteLlm(model="ollama_chat/llama3.2"),
@@ -19,3 +11,20 @@ root_agent = LlmAgent(
     instruction='Run the consume function and return how many people using kiosk and queuing for kiosk',
     tools=[consume],
 )
+
+# params = StdioServerParameters(
+#     command='uv',
+#     args=["--directory", "/home/sysadmin/AI-manageability-/kafka consumer agent/", "run", "kafka_consumer.py"]
+# )
+
+
+# root_agent = LlmAgent(
+#     model=LiteLlm(model="ollama_chat/llama3.2"),
+#     name='kafka_agent',
+#     description='A helpful assistant that help to consume kafka messages',
+#     instruction='Consume kafka message and determine how many people queuing and using',
+#     tools=[
+#         MCPToolset(
+#         connection_params=params,
+#     )],
+# )
